ebb7d0c24662dbf4a779e68bfdf5b15bbc333b49
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2023-03-12 00:09:58 +0300

    setyp pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+// react-router-dom
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// API's, react
+import { lazy, Suspense } from "react";
+// styles
+import "./App.css";
 
-import './App.css';
+// pages
+const Home = lazy(() => import("./pages/Cart/index"));
+const Login = lazy(() => import("./pages/Login/index"));
+const Products = lazy(() => import("./pages/Products/index"));
+const NotFound = lazy(() => import("./pages/NotFound/index"));
+const Cart = lazy(() => import("./pages/Cart/index"));
 
 function App() {
   return (
     <div className="App">
-    Hello
+      <Suspense fallback={"Loading..."}>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
