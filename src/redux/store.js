4aec878c5ba540ab81f1a3c9a02215a5761cda44
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2023-03-21 13:45:59 +0300

    adding redux-persist to main

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,29 @@
 // redux
 import { createStore, applyMiddleware, compose } from "redux";
+// redux-persist
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // all reducers
 import AllReducers from "./AllReducers";
 
 import thunk from "redux-thunk";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, AllReducers);
+
 // dev tool extension
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // pass reducers to store
-const store = createStore(AllReducers, {}, composeEnhancers(applyMiddleware(thunk)));
+export const store = createStore(
+  // AllReducers,
+  persistedReducer,
+  {},
+  composeEnhancers(applyMiddleware(thunk))
+);
 
-export default store;
+export const persistor = persistStore(store);
